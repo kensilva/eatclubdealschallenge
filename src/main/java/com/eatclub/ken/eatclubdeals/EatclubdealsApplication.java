52e88b04dc48ff8ece90ccacda446ca98cfff056
Ken Silva <ken.silva@accenture.com>
2025-07-08 21:57:44 +1000

    base setup

---
+++ src/main/java/com/eatclub/ken/eatclubdeals/EatclubdealsApplication.java
@@ -0,0 +1,13 @@
+package com.eatclub.ken.eatclubdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatclubdealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatclubdealsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eatclub/ken/eatclubdeals/EatclubdealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatclub.ken.eatclubdeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatclubdealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
