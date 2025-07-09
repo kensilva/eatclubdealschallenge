8c225bf64bbd4db51465a4f3f93922188a05b962
Ken Silva <ken.silva@accenture.com>
2025-07-09 10:43:38 +1000

    Modify deal service peak time for a bit cleaner. Added method details.

--- src/main/java/com/eatclub/ken/eatclubdeals/service/DealsService.java
+++ src/main/java/com/eatclub/ken/eatclubdeals/service/DealsService.java
@@ -10,6 +10,7 @@ import com.eatclub.ken.eatclubdeals.util.TimeUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,23 +42,24 @@ public class DealsService {
         return peakTimeDTO;
     }
 
+    /**
+     * Calculates the peak time
+     * @return peak time calculated.
+     */
     protected PeakTimeDTO calculatePeakTime() {
-        final List<Deal> deals = dealsRepository.getAllDeals();
+        final List<Deal> deals = dealsRepository.getAllDeals(); // sorted asc
         LocalTime peakTimeStart = null;
         LocalTime peakTimeEnd = null;
         int currentPeakTimeScore = 0;
         for (Deal deal : deals) {
             final LocalTime openTime = deal.getOpen();
-            final List<Deal> dealsWithinOpenTime = dealsRepository.findDealsWithin(openTime);
-
+            final List<Deal> dealsWithinOpenTime = dealsRepository.findDealsWithin(openTime); //sorted asc
             for (Deal withinOpenDealStart : dealsWithinOpenTime) {
                 final LocalTime closeTime = withinOpenDealStart.getClose();
-                int score = 0;
-                for (Deal withinOpenDeal : dealsWithinOpenTime) {
-                    if (!closeTime.equals(openTime) && TimeUtils.isTimeWithin(closeTime, openTime, withinOpenDeal.getClose())) {
-                        score += withinOpenDeal.getQtyLeft();
-                    }
+                if(openTime.equals(closeTime)){
+                    continue;
                 }
+                int score = calculateScore(openTime,closeTime,dealsWithinOpenTime);
                 if (score > currentPeakTimeScore) {
                     currentPeakTimeScore = score;
                     peakTimeStart = openTime;
@@ -69,6 +71,22 @@ public class DealsService {
         return new PeakTimeDTO(peakTimeStart, peakTimeEnd);
     }
 
+    /**
+     * Calculates score based on the qty where the close is within the open and each deals close time.
+     * @param open the opening time
+     * @param close the close time
+     * @param deals the deals to check. this should be filtered already
+     * @return score
+     */
+    protected int calculateScore(final LocalTime open,LocalTime close, final Collection<Deal> deals){
+        int score = 0;
+        for (Deal withinOpenDeal : deals) {
+            if (TimeUtils.isTimeWithin(close, open, withinOpenDeal.getClose())) {
+                score += withinOpenDeal.getQtyLeft();
+            }
+        }
+        return score;
+    }
 
     protected DealDTO mapToDealDTO(final Deal deal) {
         final Restaurant restaurant = deal.getRestaurant();
